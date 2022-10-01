ddc59841be5ea4904637d56c270f452111698e3e
Yusuf Yildiz <41903311+grealyve@users.noreply.github.com>
2022-10-01 09:27:13 +0300

    Delete Alakasız directory

--- Alakasız/sınav12.py
+++
@@ -1,8 +0,0 @@
-def filterList(aList):
-    list1 = []
-    for i in aList:
-        if i % 6 == 0:
-            list1.append(i)
-    return list1
-deneme = [6,12,321,21,65,60]
-print(filterList(deneme))
--- Alakasız/whazzap.py
+++
@@ -1,3 +0,0 @@
-import pywhatkit
-
-pywhatkit.sendwhatmsg("+905415345880", "Beraat kandilin mübarek olsun hacii", 0, 0)
